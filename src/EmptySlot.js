3e6243df77a4be295c9bb1678d037e2a2e7f2add
Thomas Davidson <tomd1969@gmail.com>
2023-01-11 04:49:58 -0500

    added behavior

--- src/EmptySlot.js
+++ src/EmptySlot.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+
 function EmptySlot(props) {
+    useEffect(() => {
+        let randSeconds =  Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(true)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
     return (
         <div>
             <h4>Empty Slot</h4>
